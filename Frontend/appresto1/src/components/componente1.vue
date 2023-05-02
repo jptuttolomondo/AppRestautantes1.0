d9e722e4db63b959bef391567905b5883969a07c
Juan Pablo Tuttolomondo <jptuttolomondo@hotmail.com>
2023-05-02 16:41:35 -0300

    'estrucurando_node_vue'

---
+++ Frontend/appresto1/src/components/componente1.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="hello">
+    
+      <h1>{{ texto }}</h1>
+     
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'HelloWorld',
+    props: {
+      msg: String
+    },
+    data(){
+      return{texto:'Hola Juan'}
+    }
+  }
+  </script>
+  
