7adf1ed8ae6f57372c4cf69a34d9239e5d1448bf
Juan Pablo Tuttolomondo <jptuttolomondo@hotmail.com>
2023-08-29 21:37:21 -0300

    pasando a branch development

--- Frontend/appresto1/src/reducers/login.reducer.js
+++ Frontend/appresto1/src/reducers/login.reducer.js
@@ -8,9 +8,7 @@ const initialStateLogin = {
 const loginReducer = (state = initialStateLogin, action) => {
   switch (action.type) {
      case LOGIN:
-     // state.resultLogin = action.status
-      //state.user=action.payload[0]
-      return { ...state, resultLogin: Number(action.status), user:action.payload[0] }
+          return { ...state, resultLogin: Number(action.status), user:action.payload[0] }
     case LOGOUT:
       return { ...state, resultLogin: action.payload }
     default:
--- Frontend/appresto1/src/reducers/mesas.reducer.js
+++ Frontend/appresto1/src/reducers/mesas.reducer.js
@@ -37,38 +37,22 @@ const mesasReducer = (state = initialState, action) => {
       return { ...state, mesas: action.payload };
 
     case INCREMENTAR_CANTIDAD:
-      return {
-        ...state,
-        cantidadItem: action.payload,
-        producto: state.productoItem,
-        subtotal: state.subtotalItem,
-      };
+      return {...state,cantidadItem: action.payload,producto: state.productoItem,subtotal: state.subtotalItem};
+
     case ITEM_SELECTED:
       return { ...state, itemSelected: action.payload };
 
     case DECREMENTAR_CANTIDAD:
-      return {
-        ...state,
-        cantidadItem: action.payload,
-        producto: state.productoItem,
-        subtotal: state.subtotalItem,
-      };
+      return {...state,cantidadItem: action.payload,producto: state.productoItem,subtotal: state.subtotalItem};
 
     case ACTUALIZAR_COMANDA_MESA:
       return { ...state, MesaComanda: action.payload };
 
     case ACTUALIZAR_PRODUCTO:
-      return {
-        ...state,
-        productoItem: action.payload,
-        precioItem: action.payload[0].precio,
-      };
+      return {...state,productoItem: action.payload,precioItem: action.payload[0].precio};
 
     case CALCULAR_SUBTOTAL:
-      return {
-        ...state,
-        subtotalItem: Number(state.cantidadItem) * Number(state.precioItem),
-      };
+      return {...state,subtotalItem: Number(state.cantidadItem) * Number(state.precioItem)};
 
     case CALCULAR_TOTAL:
       return { ...state, total: Number(action.payload) };
